ed25a5db0538c123056a0cbc565578af98e775a5
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-04-10 20:52:31 +0700

    feat(App): add components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import ButtonGradient from './assets/svg/ButtonGradient.jsx'
-import Button from './components/Button.jsx'
 import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
+import Project from './components/Project.jsx'
+import Skills from './components/Skills.jsx'
+import Devtools from "./components/Devtools.jsx"
+import Contacts from './components/Contacts.jsx'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
-  return (
-    <>
-        <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-            <Header />
-        </div>
-        <ButtonGradient /> 
-    </>
-  )
+    return (
+        <>
+            <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+                <Header />
+                <Hero />
+                <Project />
+                <Skills />
+                <Devtools />
+                <Contacts />
+                <Footer />
+            </div>
+
+            <ButtonGradient /> 
+        </>
+    )
 }
 
 export default App
