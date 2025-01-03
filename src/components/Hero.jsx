d49fd42a33015e0d6d1fcc1114f5b40c8405813d
wreckitral <defhanayasofhiea@gmail.com>
2025-01-03 12:52:03 +0700

    feat: add the hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
             id="hero">
             <div className="container relative" ref={parallaxRef}>
                 <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[1rem] md:mb-5 lg:mb-[2.25rem]">
-                    <h1 className="h1 mb-6">Hall of {""} 
+                    <h1 className="h1 mb-6">Hall of {""}
                         <span className="inline-block relative">Projects
                             <img
                                 src={curve}
@@ -30,11 +30,11 @@ const Hero = () => {
                         </span> By Defhanaya
                     </h1>
                     <p className="body-1 max-w-3xl mx-auto mb-[6rem] text-n-2 lg:mb-[8rem] xl:mb-[7rem]">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        I’m a back-end developer with 2 years of experience and a Computer Science student at Sriwijaya University.
+                        I enjoy creating RESTful APIs, building data-heavy apps, and working with cloud services.
+                        I’m comfortable managing both SQL and NoSQL databases and coding in Go, JavaScript, and Python.
+                        I love learning new things, building IoT projects, and helping others with code consultations on campus.
+                        Outside of tech, I like to unwind with some sports for fun!
                     </p>
                     <Button href="/contacts" white>
                         Reach Me
@@ -51,11 +51,11 @@ const Hero = () => {
                             alt="hero"
                         />
                     </div>
-                                        
+
                     <BackgroundCircles />
                 </div>
-                
-                <Moto className="hidden relative z-10 mt-20 lg:block" /> 
+
+                <Moto className="hidden relative z-10 mt-20 lg:block" />
             </div>
 
             <BottomLine />
